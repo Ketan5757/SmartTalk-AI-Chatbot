207f6c721d3cc097c405b1c291b406b96132d69f
Ketan Darekar <152967914+Ketan5757@users.noreply.github.com>
2025-01-29 13:13:27 +0100

    Created dashboard page with chat interface UI.
    - Designed the UI for a chat interface where users can input texts, documents, and more.
    - Focused on building the layout and user interaction elements.
    - Only the UI for now, without the backend functionality.

--- src/routes/dashboardPage/DashboardPage.jsx
+++ src/routes/dashboardPage/DashboardPage.jsx
@@ -2,7 +2,35 @@ import './dashboardPage.css'
 
 const DashboardPage  = () => {
     return(
-        <div className='dashboardPage'>DashboardPage</div>
+        <div className='dashboardPage'>
+            <div className="texts">
+                <div className="logo">
+                    <img src="/logo1.png" alt="" />
+                    <h1>SmartTalk-AI</h1>
+                </div>
+                <div className="options">
+                    <div className="option">
+                        <img src="chat.png" alt="" />
+                        <span>Start a new Chat</span>
+                    </div>
+                    <div className="option">
+                        <img src="image.jpg" alt="" />
+                        <span>Analyze Documents & Images</span>
+                    </div>
+                    <div className="option">
+                        <img src="code.png" alt="" />
+                        <span>Help me with my code</span>
+                    </div>
+                </div>
+            </div>
+            <div className="formContainer"><form >
+                <input type="text" placeholder='Ask me anything...' />
+                <button>
+                    <img src="arrow.png" alt="" />
+                </button>
+                
+                </form></div>
+        </div>
     )
 }
 
