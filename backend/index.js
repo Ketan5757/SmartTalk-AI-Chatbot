2e8b13ddf98ee6ff4f1966a956c4570db190ab9b
Ketan Darekar <152967914+Ketan5757@users.noreply.github.com>
2025-02-13 12:31:08 +0100

    Connected MongoDB using Mongoose via env file & implemented chat history storage
    - Configured MongoDB connection using Mongoose with environment variables
    - Added functionality to store and maintain user chat history in the database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import ImageKit from "imagekit";
+import mongoose from "mongoose";
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -9,6 +10,18 @@ app.use(
     cors({
     origin: process.env.CLIENT_URL,
 }));
+
+app.use(express.json())
+
+const connect = async ()=>{
+    try{
+        await mongoose.connect(process.env.MONGO)
+        console.log("Connected to MongoDB")
+
+    }catch(err){
+        console.log(err)
+    }
+}
 
 const imagekit = new ImageKit({
     urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
@@ -20,7 +33,12 @@ app.get("/api/upload",(req,res)=>{
     const result = imagekit.getAuthenticationParameters();
     res.send(result);
 })
+app.post("/api/chats",(req,res)=>{
+    const{text} = req.body
+    console.log(text)
+})
 
 app.listen(port,()=>{
+    connect()
     console.log("Server running on 3000");
 });
--- client/src/components/newPrompt/NewPrompt.jsx
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -63,7 +63,7 @@ const NewPrompt  = () => {
 
         add(text)
 
-    }
+    };
 
     return(
         <>
